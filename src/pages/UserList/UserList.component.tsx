8821979a94b1aec5f5ac3379cceda871684118df
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-14 16:05:38 +0800

    feat(user-list): implement select input component on user list page

--- src/pages/UserList/UserList.component.tsx
+++ src/pages/UserList/UserList.component.tsx
@@ -4,10 +4,17 @@ import api from "../../api";
 import { User as UserFromAPI } from "../../api/randomUser/randomUser.api.types";
 import Button from "../../components/Button";
 import PaginationBar from "../../components/PaginationBar";
+import SelectInput from "../../components/SelectInput";
+import { Option } from "../../components/SelectInput/SelectInput.component.types";
 import TextInput from "../../components/TextInput";
 import UserTable from "../../components/UserTable";
 import { User } from "../../index.types";
 import { SetUsers } from "./UserList.component.types";
+
+const OPTIONS_FILTER:Option[] = [
+  {label:'Female', value: 'female'},
+  {label:'Male', value: 'Male'},
+]
 
 const _convertUserToUserState = (user: UserFromAPI):User => ({
   email: user.email,
@@ -42,9 +49,10 @@ const UserList = () =>{
   useInnit(setUsers)
   return (
     <div>
-      <TextInput id='search-user-input' placeholder="Search.." type={'search'}></TextInput>
+      <TextInput id='search-user-input' placeholder="Search.." type={'search'}/>
       <Button label="Search"/>
       <Button label="Reset Filter"/>
+      <SelectInput options={OPTIONS_FILTER}/>
       <UserTable users={users}/>
       <PaginationBar currentIndex={1} totalPages={3}/>
     </div>
