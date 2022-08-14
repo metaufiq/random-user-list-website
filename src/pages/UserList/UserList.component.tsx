424387845fe532c15e9014a6cd5a2f4ec333b44f
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-14 17:10:09 +0800

    feat(user-list): refactor user list page

--- src/pages/UserList/UserList.component.tsx
+++ src/pages/UserList/UserList.component.tsx
@@ -1,64 +1,26 @@
-import React, { ChangeEventHandler, useEffect, useState } from "react";
+import React from "react";
 
-import api from "../../api";
-import { User as UserFromAPI } from "../../api/randomUser/randomUser.api.types";
 import Button from "../../components/Button";
 import PaginationBar from "../../components/PaginationBar";
 import SelectInput from "../../components/SelectInput";
 import { Option } from "../../components/SelectInput/SelectInput.component.types";
 import TextInput from "../../components/TextInput";
 import UserTable from "../../components/UserTable";
-import { User } from "../../index.types";
-import { SetSearchInput, SetUsers } from "./UserList.component.types";
+import useUserQuery from "../../hooks/useUserQuery/useUserQuery.hooks";
+import { SetSearchInput } from "../../hooks/useUserQuery/useUserQuery.hooks.types";
 
 const OPTIONS_FILTER:Option[] = [
   {label:'Female', value: 'female'},
   {label:'Male', value: 'Male'},
 ]
 
-const _convertUserToUserState = (user: UserFromAPI):User => ({
-  email: user.email,
-  gender: user.gender,
-  name: `${user.name.first} ${user.name.last}`,
-  username: user.login.username,
-  regisDate: user.registered.date
-})
-
-const _getListUser = async (params: any):Promise<User[]> => {
-  const {results} = await api.randomUser.getListUser(params)
-
-  const users = results.map(_convertUserToUserState)
-  return users;
-}
-
-const _setUsersState = async (setUsers: SetUsers, params: any)=>{
-  const users = await _getListUser(params);
-  
-  setUsers(users)
-}
-
-const useInnit = (setUsers: SetUsers)=>{
-  useEffect(()=>{
-    _setUsersState(setUsers, {})
-  }, []);
-}
-
-const useSearch = (searchInput:string, setUsers: SetUsers)=>{
-  useEffect(()=>{
-    _setUsersState(setUsers, {keyword: searchInput})
-  }, [searchInput]);
-}
-
 const _onSearchChange=(setSearchInput: SetSearchInput)=>(event: React.ChangeEvent<HTMLInputElement>)=>{
   setSearchInput(event.target.value)
 }
 
 const UserList = () =>{
-  const [users, setUsers] = useState<User[]>([]);
-  const [searchInput, setSearchInput] = useState('');
+  const {users, setSearchInput} = useUserQuery();
 
-  useInnit(setUsers)
-  useSearch(searchInput, setUsers)
   return (
     <div>
       <TextInput id='search-user-input' placeholder="Search.." type={'search'} onChange={_onSearchChange(setSearchInput)}/>
--- src/pages/UserList/UserList.component.types.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../../index.types'
-
-export type SetUsers = React.Dispatch<React.SetStateAction<User[]>>;
-
-export type SetSearchInput = React.Dispatch<React.SetStateAction<string>>;
