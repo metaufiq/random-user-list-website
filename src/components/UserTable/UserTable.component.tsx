ef56e6aa4dafb077ad1fd6aead04cb9ac48e90ce
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-15 11:33:29 +0800

    feat(user-list): implement reducer logic to usertable components

--- src/components/UserTable/UserTable.component.tsx
+++ src/components/UserTable/UserTable.component.tsx
@@ -1,32 +1,58 @@
+import { useReducer } from "react";
+
 import { SortIcon, SortIconAsc, SortIconDesc } from "../Icons";
-import { Props } from "./UserTable.component.types";
+import reducer, { initialState } from "./UserTable.component.reducer";
+import { DataWithLabel, Props, ReducerState, SortBy } from "./UserTable.component.types";
+
+const DATA_WITH_LABEL: DataWithLabel = {
+  username: 'Username',
+  name: 'Name',
+  email: 'Email',
+  gender: 'Gender',
+  regisDate: 'Registered Date'
+}
+
+const DATA_WITH_LABEL_OBJECT = Object.keys(DATA_WITH_LABEL).map(key=>key)
+
+const _getIconByReducerState = ({using, condition}: ReducerState, key: string)=>{
+  if (using !== key) return SortIcon
+  switch (condition) {
+    case 'ASCEND':
+      return SortIconAsc
+    
+    case 'DESCEND':
+      return SortIconDesc
+    default:
+      return SortIcon
+  }
+}
+
+const _renderIcon = (reducerState: ReducerState, key: SortBy)=>{
+  const Icon = _getIconByReducerState(reducerState, key)
+
+  return (<Icon size={12}/>)
+}
+
+const _renderHeader = (reducerState: ReducerState)=>((key:SortBy)=>(
+  <th scope="col" className="py-3 px-6" key={key}>
+    {DATA_WITH_LABEL[key]} {_renderIcon(reducerState, key)}
+  </th>
+))
 
 const UserTable = ({users}: Props)=>{
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
   return(
   <div className="overflow-x-auto relative">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                  <th scope="col" className="py-3 px-6">
-                      Username <SortIcon size={12}/>
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                      Name  <SortIconDesc size={13}/>
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                      Email <SortIconAsc size={13}/>
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                      Gender <SortIcon size={12}/>
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                      Registered Date <SortIcon size={12}/>
-                  </th>
+                {DATA_WITH_LABEL_OBJECT.map((value: string)=>_renderHeader(state)(value as SortBy))}
               </tr>
           </thead>
           <tbody>
               {users.map(user=>(
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={user.email}>
                   <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                       {user.username}
                   </th>
--- src/components/UserTable/UserTable.component.types.ts
+++ src/components/UserTable/UserTable.component.types.ts
@@ -3,3 +3,21 @@ import { User } from "../../index.types"
 export type Props = {
   users: User[]
 }
+
+export type SortCondition = 'ASCEND' | 'DESCEND' | undefined
+
+export type SortBy = keyof User;
+
+export type DataWithLabel = {
+  [key in keyof User]: string;
+};
+
+export type ReducerState = {
+  using?: SortBy,
+  condition?: SortCondition
+}
+
+export type ReducerAction = {
+  type: SortCondition
+  payload: SortBy 
+}
