896d716a3ab6fca9daf9371f37f53d9f2b9ac26c
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-15 21:39:22 +0800

    chore(user-list): add Icons snapshot

---
+++ src/components/Icons/index.snap.test.tsx
@@ -0,0 +1,78 @@
+import { SortIcon, SortIconAsc, SortIconDesc } from ".";
+import { assertSnapshots } from "../../../test";
+import { Props } from "./index.types";
+
+describe('SortIconSnapshot', () => {
+  const sizeOnlyProps:Props = {
+    size: 2
+  };
+
+  const props:Props = {
+    size: 2,
+    color: 'red'
+  }
+
+  const configs = [
+    {
+      props,
+      desc: 'should render normal icon'
+    },
+    {
+      props: sizeOnlyProps,
+      desc: 'should render icon with custom color'
+    },
+
+  ];
+
+  assertSnapshots(SortIcon, configs);
+});
+
+describe('SortIconAscSnapshot', () => {
+  const sizeOnlyProps:Props = {
+    size: 2
+  };
+
+  const props:Props = {
+    size: 2,
+    color: 'red'
+  }
+
+  const configs = [
+    {
+      props,
+      desc: 'should render normal icon'
+    },
+    {
+      props: sizeOnlyProps,
+      desc: 'should render icon with custom color'
+    },
+
+  ];
+
+  assertSnapshots(SortIconAsc, configs);
+});
+
+describe('SortIconDescSnapshot', () => {
+  const sizeOnlyProps:Props = {
+    size: 2
+  };
+
+  const props:Props = {
+    size: 2,
+    color: 'red'
+  }
+
+  const configs = [
+    {
+      props,
+      desc: 'should render normal icon'
+    },
+    {
+      props: sizeOnlyProps,
+      desc: 'should render icon with custom color'
+    },
+
+  ];
+
+  assertSnapshots(SortIconDesc, configs);
+});
