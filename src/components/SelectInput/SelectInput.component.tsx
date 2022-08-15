3d57fd4688fd2df28a64c5b2ca8b4c38429eba31
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-15 23:05:31 +0800

    fix(user-list): remove dark mode select inpu

--- src/components/SelectInput/SelectInput.component.tsx
+++ src/components/SelectInput/SelectInput.component.tsx
@@ -13,7 +13,7 @@ const SelectInput = ({options, label, htmlFor, id, onChange, value}: Props)=>{
         </label>}
       <select 
         id={id} 
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         value={value}
         onChange={onChange}>
         {options.map(option=><option value={option.value}>{option.label}</option>)}
