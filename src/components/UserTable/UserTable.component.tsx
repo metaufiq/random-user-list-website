8d948b0bfc22635ac5c7a13034068f42d09608d5
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-15 23:05:50 +0800

    fix(user-list): remove dark mode user table

--- src/components/UserTable/UserTable.component.tsx
+++ src/components/UserTable/UserTable.component.tsx
@@ -76,16 +76,16 @@ const UserTable = (props: Props)=>{
 
   return(
   <div className="overflow-x-auto relative">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
               <tr>
                 {DATA_WITH_LABEL_OBJECT.map((value: string)=>_renderHeader(state, dispatch)(value as SortBy))}
               </tr>
           </thead>
           <tbody>
               {props.users.map(user=>(
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={user.email}>
-                  <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr className="bg-white border-b" key={user.email}>
+                  <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
                       {user.username}
                   </th>
                   <td className="py-4 px-6">
