22a36bce35ad1a93e00898db8545ef1c3f0a04bb
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-16 11:41:59 +0800

    fix(user-list): add roundedType props to text input component

--- src/components/TextInput/TextInput.component.types.ts
+++ src/components/TextInput/TextInput.component.types.ts
@@ -1,8 +1,11 @@
 import { ChangeEventHandler, HTMLInputTypeAttribute } from "react"
 
+import { RoundedType } from "../../../test/index.types"
+
 export type Props = {
   placeholder: string,
   id?: string,
   type: HTMLInputTypeAttribute,
+  roundedType?: RoundedType,
   onChange: ChangeEventHandler<HTMLInputElement>
 }
