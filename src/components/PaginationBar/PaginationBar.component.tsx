10af74afc535518bf1a95b16c5cae0bc7c23d5de
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-16 12:11:45 +0800

    fix(user-list): PaginationBar component UI

--- src/components/PaginationBar/PaginationBar.component.tsx
+++ src/components/PaginationBar/PaginationBar.component.tsx
@@ -5,7 +5,7 @@ const _renderActiveButton = (props:Props, index: number) => (
     <button 
       onClick={props.onChange(index)} 
       aria-current="page" 
-      className="py-2 px-3 leading-tight text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700"
+      className="py-2 px-3 mx-1 leading-tight text-blue-400 border border-blue-400 hover:bg-blue-50 hover:text-blue-500 hover:border-blue-500"
       >
         {index}
     </button>
@@ -16,7 +16,7 @@ const _renderInActiveButton = (props:Props, index: number) => (
   <li key={index}>
     <button 
       onClick={props.onChange(index)} 
-      className={`py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700`}
+      className={`py-2 px-3 mx-1 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700`}
       >
         {index}
     </button>
@@ -28,7 +28,7 @@ const _renderPreviousButton = (props: Props) => {
     <li>
       <button 
         onClick={props.onChange(props.currentIndex-1)} 
-        className="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+        className="mr-1 py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
       >
         Previous
       </button>
@@ -40,7 +40,7 @@ const _renderNextButton = (props: Props) => (
   <li>
     <button 
       onClick={props.onChange(props.currentIndex+1)} 
-      className="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+      className="ml-1 py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-sm border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
     >
       Next
     </button>
