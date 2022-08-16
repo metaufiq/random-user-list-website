e52245a82f69924bfb7e6083dd068dbe2f92ac76
Muh.Taufiqulsadi <mtaufiqulsadi@gmail.com>
2022-08-16 12:20:47 +0800

    feat(user-list): update SortIcon components to accept color

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -5,7 +5,8 @@ export const SortIcon = (props: Props) => (
     xmlns="http://www.w3.org/2000/svg" 
     width={props.size} 
     height={props.size} 
-    viewBox="0 0 24 24">
+    viewBox="0 0 24 24"
+    fill={props.color}>
       <path 
         d="M12 0l8 10h-16l8-10zm8 14h-16l8 10 8-10z"/>
   </svg>
@@ -16,7 +17,8 @@ export const SortIconDesc = (props: Props) => (
     xmlns="http://www.w3.org/2000/svg" 
     width={props.size} 
     height={props.size}
-    viewBox="0 0 24 24">
+    viewBox="0 0 24 24"
+    fill={props.color}>
       <path 
         d="M12 3.202l3.839 4.798h-7.678l3.839-4.798zm0-3.202l-8 10h16l-8-10zm8 14h-16l8 10 8-10z"
       />
@@ -28,7 +30,8 @@ export const SortIconAsc = (props: Props) => (
     xmlns="http://www.w3.org/2000/svg" 
     width={props.size} 
     height={props.size}
-    viewBox="0 0 24 24">
+    viewBox="0 0 24 24"
+    fill={props.color}>
       <path 
         d="M12 0l-8 10h16l-8-10zm3.839 16l-3.839 4.798-3.839-4.798h7.678zm4.161-2h-16l8 10 8-10z"
       />
